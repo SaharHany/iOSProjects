fcb901baad014358657ee99ecc51c16c36cf618d
Sahar96Hany <sahar96hany@gmail.com>
2020-07-04 20:29:48 +0200

    no message

--- BioAuth/BioAuth/ViewController.swift
+++ BioAuth/BioAuth/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
+    var context  =  LAContext()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        context.localizedCancelTitle   = "End Session"
+        context.localizedFallbackTitle = "Use Passcode (2)"
+        context.localizedReason        = "The app needs your authentication"
+        evaluatePolicy()
     }
 
 
+    func evaluatePolicy() {
+        var errorCanEval : NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &errorCanEval) {
+            // incase of can Evaluate policy
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Fallback title - override reason ") { success, error in
+                print(success)
+                
+                if let err = error {
+                    let evalErrorCode = LAError(_nsError: err as NSError)
+                    switch evalErrorCode.code {
+                    case LAError.Code.userCancel:
+                        print("user cancelled")
+                    case LAError.Code.userFallback:
+                        print("fallback")
+                    case LAError.Code.authenticationFailed:
+                        print("failed to authenticate")
+                    default:
+                        print("other error")
+                    }
+                    
+                }
+            }
+        }else{
+            //incase of can't evaluate policy
+            print("Cannot evaluate Policy...")
+            print(errorCanEval?.localizedDescription ?? "no error description")
+        }
+    }
 }
 
