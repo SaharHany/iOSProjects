765534b40c7b371f0dd7d07bade58e33355c5010
Sahar96Hany <sahar96hany@gmail.com>
2020-07-04 21:23:15 +0200

    initial commit of WeatherReporter App

---
+++ WeatherReporter/WeatherReporter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherReporter
+//
+//  Created by Sahar Hany on 7/4/20.
+//  Copyright © 2020 Studying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
